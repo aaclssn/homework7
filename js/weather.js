dd2165178ecfbb4087fb0cbc5f7b2f315d30ebc5
aaclssn <54865694+aaclssn@users.noreply.github.com>
2020-12-07 19:53:56 -0500

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -1,31 +1,47 @@
 function gettingJSON(){
     //Display the forecast
     // Your code here.
+    document.querySelector('#forecast').style.display = "block";
 
     //Set default location if one isn't provided
     let location;
-
-    // Your code here.
+    //default location Ann Arbor
+    location = document.querySelector("#location").value;
+    if (location == "") {
+        location = "Ann Arbor, US";
+        console.log("Location is : " + location);
+    }
+    
     console.log("Location is : " + location);
+   
 
     //set default temperature format if one isn't provided
-    let fomat;
-
+    let format;
+        format = "imperial";
+        if(document.querySelector("#fahrenheit").checked) {
+            format = "imperial";
+        }
+        else if(document.querySelector("#celcius").checked) {
+            format = "metric";
+            
+        }
     // Your code here.
-    console.log("Format is " + format);
-
-    //set the query  
-    let query;
-    // Your code here.  
     
+
+    //set the query 
+    let query; 
+    keyhw = "04d72a5ec79e2e14a507f103851831aa";
+    query = "https://api.openweathermap.org/data/2.5/weather?q=" + location + "&units=" + format + "&appid=" + keyhw;
     console.log("Query is :" + query);
+
+  
 
     //Create and set variables for each of the elements you
     //need to update, location, temp, the image, etc.
 
-    let loc;
-    let temp;
-    let tempImg;
+    let loc = document.querySelector("#loc");
+    let temp = document.querySelector("#temp");
+    let tempImg = document.querySelector("#tempImg");
     // Your code here.
 
 
@@ -34,6 +50,13 @@ function gettingJSON(){
         //elements in HTML.  
         //I would print the JSON to the console
         // Your code here.
-
+        console.log(JSON.stringify(json));
+        loc.innerHTML = json["name"];
+		temp.innerHTML = json["main"]["temp"] + " with " + json["weather"][0]["description"];
+		tempImg.src = "http://openweathermap.org/img/wn/"+ json["weather"][0]["icon"] + ".png";
+		tempImg.alt = "Weather icon for "+ json["weather"][0]["description"];
+        console.log("updated weather values should now be displayed")
     });
+    
+
 }
